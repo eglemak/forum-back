34defd2bceac8eaf5a53a33f7786efa09fd5cb37
eglemak <egle.laseviciute@gmail.com>
2023-06-28 20:02:17 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+var cors = require("cors");
+const bodyParser = require("body-parser");
+
+// const ticketRouter = require("./routes/ticket");
+// const userRouter = require("./routes/user");
+require("dotenv").config();
+const mongoose = require("mongoose");
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// app.use(ticketRouter);
+// app.use(userRouter);
+
+mongoose
+  .connect(process.env.MONGO_CONNECT)
+  .then(() => {
+    console.log("CONNECTED");
+  })
+  .catch((err) => {
+    console.log("err", err);
+  });
+
+app.listen(process.env.PORT, () => {
+  console.log("Your app is alive!!!!!");
+});
